4b6722bc7c325e7c08ba94ec570d754cccc40362
Feruzk <38233905+Feruzk@users.noreply.github.com>
2021-09-14 19:18:24 +0900

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -13,17 +13,4 @@ Header.defaultProps = {
   title: 'Shopping List',
 };
 
-const styles = StyleSheet.create({
-  header: {
-    height: 60,
-    padding: 15,
-    backgroundColor: 'darkslateblue',
-  },
-  text: {
-    color: 'white',
-    fontSize: 23,
-    textAlign: 'center',
-  },
-});
-
 export default Header;
