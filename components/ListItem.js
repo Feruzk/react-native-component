e7730897cfc6a8cdda1ff6351f0ae35e727925de
Feruzk <38233905+Feruzk@users.noreply.github.com>
2021-09-19 22:22:09 +0900

    Update ListItem.js

--- components/ListItem.js
+++ components/ListItem.js
@@ -70,35 +70,4 @@ const ListItem = ({
   );
 };
 
-const styles = StyleSheet.create({
-  listItem: {
-    padding: 15,
-    backgroundColor: '#f8f8f8',
-    borderBottomWidth: 1,
-    borderColor: '#eee',
-  },
-  listItemView: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  listItemText: {
-    fontSize: 18,
-  },
-  checkedItemText: {
-    fontSize: 18,
-    textDecorationLine: 'line-through',
-    color: 'green',
-  },
-  iconView: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: 70,
-  },
-  editItemInput: {
-    padding: 0,
-    fontSize: 18,
-  },
-});
-
 export default ListItem;
