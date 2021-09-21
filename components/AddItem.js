555c46ab7fb4ba2e5cb01b5c2f6e141c34d0154e
Feruzk <38233905+Feruzk@users.noreply.github.com>
2021-09-21 22:06:15 +0900

    Update AddItem.js

--- components/AddItem.js
+++ components/AddItem.js
@@ -35,21 +35,7 @@ const AddItem = ({addItem}) => {
 };
 
 const styles = StyleSheet.create({
-  input: {
-    height: 60,
-    padding: 8,
-    margin: 5,
-  },
-  btn: {
-    backgroundColor: '#c2bad8',
-    padding: 9,
-    margin: 5,
-  },
-  btnText: {
-    color: 'darkslateblue',
-    fontSize: 20,
-    textAlign: 'center',
-  },
+ 
 });
 
 export default AddItem;
